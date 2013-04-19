ec42d15f6d14aaf844dd0e9471343fad69d2130e
William Stuart <williamstuart@westminster.net>
2013-04-19 13:10:15 -0400

    Functional
    It now works for SMA's but is very dirty

---
+++ src/CleanedStock.java
@@ -0,0 +1,8 @@
+
+public class CleanedStock {
+
+	public CleanedStock() {
+		
+	}
+
+}
--- src/Stock.java
+++ src/Stock.java
@@ -4,51 +4,71 @@ import java.util.*;
 public class Stock {
 	
 	public static void main(String[] args) throws FileNotFoundException {
-		System.out.println((SMAPoint(listCreator("MSFTQuotes"),0)));
+		Stock MSFT;
+		MSFT = new Stock("MSFT", prices);
+		for(int i=101; i<2000; i++){
+			decision(MSFT, i);
+			System.out.println(accountValue(i, MSFT));
+		}
+		System.out.println(prices.size());
 	}
+	
 	
 	static ArrayList<Double> prices = new ArrayList<Double>();
 	
+
 	String name = null;
-	boolean investedIn=false;
+	static boolean investedIn=false;
+	static double accountCash = 100000;
+	static double shares=0;
 	
-	public Stock(String name){
+	public Stock(String name, ArrayList<Double> prices) throws FileNotFoundException{
 		this.name=name;
-		
+		this.prices=listCreator(name);
 	}
 	
-	public void decision(int date, Stock stock){
+	public static void decision(Stock stock, int date){
 		if(SMAPoint(prices, date)<=prices.get(date)){
-			if(stock.investedIn=false){
-				buy(date);
+			if(!investedIn){
+				buy(date, stock);
 			}
 		}
 		else{
-			sell(date);
+			if(investedIn){
+			sell(date, stock);
+			}
 		}
 	}
 
-	public void buy(int date) {
+	public static void buy(int date, Stock stock) {
 		investedIn=true;
+		shares=accountCash/stock.prices.get(date);
+		accountCash=0;
+		System.out.println("Bought");
 	}
-	public void sell(int date){
+	public static void sell(int date, Stock stock){
 		investedIn=false;
+		accountCash=shares*stock.prices.get(date);
+		shares=0;
+		System.out.println("Sold");
 	}
-	public double currentValue(Stock stock, int date){
-		
+	public static double accountValue(int date, Stock stock){
+		return accountCash+shares*stock.prices.get(date);
+	}
+	public static double currentValue(Stock stock, int date){
+		return stock.prices.get(date);
 	}
 	//TODO: Aggregate many files
-	
 	public static ArrayList<Double> listCreator(String stock) throws FileNotFoundException{
 		int i=0;
-		Scanner sc = new Scanner(new File(stock+".csv"));
+		Scanner sc = new Scanner(new File(stock+"Quotes.csv"));
 		sc.useDelimiter("\",\"");
 		sc.nextLine();
 		while(sc.hasNextLine()){
-		sc.next();
-		prices.add(Double.parseDouble(sc.next()));
-		//System.out.println(prices.get(i));
 		i++;
+		sc.next();
+		prices.add(0, Double.parseDouble(sc.next()));
+		//System.out.println(prices.get(i));
 		sc.nextLine();
 		}
 		return prices;
@@ -56,8 +76,8 @@ public class Stock {
 	public static double SMAPoint(ArrayList<Double> prices, int date){
 		double finalPrice=0;
 		for (int i = 0; i<20; i++){
-			finalPrice += prices.get(date+(i*5));
-			System.out.println(finalPrice);
+			finalPrice += prices.get(date-(i*5));
+			//System.out.println(finalPrice);
 		}
 		return finalPrice/20;
 	}
